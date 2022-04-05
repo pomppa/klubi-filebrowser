caee7a39792133571a29f0c45464e444b17b6b75
Tuomas Änäkkälä <tuomas.anakkala@vaimo.com>
2022-04-05 15:42:40 +0300

    Handle login errors

--- src/LoginContainer.js
+++ src/LoginContainer.js
@@ -3,11 +3,19 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './LoginButton';
 
 function LoginContainer() {
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading, error } = useAuth0();
   const url = process.env.REACT_APP_BASE_URL;
 
   if (isLoading) {
     return <div />;
+  }
+
+  if (error) {
+    return (
+      <div className="my-40 max-w-2xl mx-auto bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h1 className="my-2 text-xl text-white">You were not allowed to log in.</h1>
+      </div>
+    );
   }
 
   return !isAuthenticated
